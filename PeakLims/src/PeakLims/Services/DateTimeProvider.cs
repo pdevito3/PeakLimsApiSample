d9f4f1801c712690a06d90ee64073dd77987cd84
Paul DeVito <pdevito3@gmail.com>
2022-10-15 09:58:42 -0400

    update: simplify date time provider

--- PeakLims/src/PeakLims/Services/DateTimeProvider.cs
+++ PeakLims/src/PeakLims/Services/DateTimeProvider.cs
@@ -3,17 +3,11 @@ namespace PeakLims.Services;
 public interface IDateTimeProvider
 {
     DateTime DateTimeUtcNow { get; }
-    DateTime DateTimeNow { get; }
-    DateTimeOffset DateTimeOffsetNow { get; }
     DateOnly DateOnlyUtcNow { get; }
-    DateOnly DateOnlyNow { get; }
 }
 
 public class DateTimeProvider : IDateTimeProvider
 {
     public DateTime DateTimeUtcNow => DateTime.UtcNow;
-    public DateTime DateTimeNow => DateTime.Now;
-    public DateTimeOffset DateTimeOffsetNow => DateTimeOffset.Now;
     public DateOnly DateOnlyUtcNow => DateOnly.FromDateTime(DateTimeUtcNow);
-    public DateOnly DateOnlyNow => DateOnly.FromDateTime(DateTimeNow);
 }
