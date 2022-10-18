2b176000e3a29286f2fd2142ca1f85275332e27e
Paul DeVito <pdevito3@gmail.com>
2022-10-17 22:25:37 -0400

    update: add missing unit tests for accession

--- PeakLims/tests/PeakLims.UnitTests/UnitTests/Domain/Accessions/ManageHealthcareOrganizationOnAccessionTests.cs
+++ PeakLims/tests/PeakLims.UnitTests/UnitTests/Domain/Accessions/ManageHealthcareOrganizationOnAccessionTests.cs
@@ -2,9 +2,15 @@ namespace PeakLims.UnitTests.UnitTests.Domain.Accessions;
 
 using Bogus;
 using FluentAssertions;
+using Moq;
 using NUnit.Framework;
 using PeakLims.Domain.Accessions;
+using Services;
+using SharedTestHelpers.Fakes.Accession;
+using SharedTestHelpers.Fakes.Container;
 using SharedTestHelpers.Fakes.HealthcareOrganization;
+using SharedTestHelpers.Fakes.Patient;
+using SharedTestHelpers.Fakes.Sample;
 
 [Parallelizable]
 public class ManageHealthcareOrganizationOnAccessionTests
@@ -36,5 +42,52 @@ public class ManageHealthcareOrganizationOnAccessionTests
         // Assert - Remove
         accession.HealthcareOrganization.Should().BeNull();
         accession.HealthcareOrganizationId.Should().BeNull();
+    }
+
+    [Test]
+    public void can_not_add_inactive_org()
+    {
+        // Arrange
+        var accession = Accession.Create();
+        var healthcareOrganization = FakeHealthcareOrganization.Generate();
+        healthcareOrganization.Deactivate();
+        
+        // Act - add
+        var act = () => accession.SetHealthcareOrganization(healthcareOrganization);
+
+        // Assert - Add
+        act.Should().Throw<SharedKernel.Exceptions.ValidationException>()
+            .WithMessage("Only active organizations can be set on an accession.");
+    }
+
+    [Test]
+    public void can_not_update_org_when_processing_accession()
+    {
+        // Arrange
+        var patient = FakePatient.Generate(Mock.Of<IDateTimeProvider>());
+        var org = FakeHealthcareOrganization.Generate();
+        var accession = FakeAccessionBuilder
+            .Initialize()
+            .WithMockTestRepository()
+            .WithPatient(patient)
+            .WithHealthcareOrganization(org)
+            .Build();
+        var container = FakeContainer.Generate();
+        var sample = FakeSample.Generate(container);
+        accession.TestOrders.FirstOrDefault().SetSample(sample);
+        accession.DomainEvents.Clear();
+        accession.SetStatusToReadyForTesting(Mock.Of<IDateTimeProvider>());
+
+        var anotherOrg = FakeHealthcareOrganization.Generate();
+        
+        // Act
+        var actAdd = () => accession.SetHealthcareOrganization(anotherOrg);
+        var actRemove = () => accession.RemoveHealthcareOrganization();
+
+        // Assert
+        actAdd.Should().Throw<SharedKernel.Exceptions.ValidationException>()
+            .WithMessage($"This accession is processing. The organization can not be modified.");
+        actRemove.Should().Throw<SharedKernel.Exceptions.ValidationException>()
+            .WithMessage($"This accession is processing. The organization can not be modified.");
     }
 }
--- PeakLims/tests/PeakLims.UnitTests/UnitTests/Domain/Accessions/ManagePatientOnAccessionTests.cs
+++ PeakLims/tests/PeakLims.UnitTests/UnitTests/Domain/Accessions/ManagePatientOnAccessionTests.cs
@@ -7,8 +7,11 @@ using Moq;
 using NUnit.Framework;
 using PeakLims.Domain.Accessions;
 using Services;
+using SharedTestHelpers.Fakes.Container;
+using SharedTestHelpers.Fakes.HealthcareOrganization;
 using SharedTestHelpers.Fakes.HealthcareOrganizationContact;
 using SharedTestHelpers.Fakes.Patient;
+using SharedTestHelpers.Fakes.Sample;
 
 [Parallelizable]
 public class ManagePatientOnAccessionTests
@@ -40,5 +43,36 @@ public class ManagePatientOnAccessionTests
         // Assert - Remove
         accession.Patient.Should().BeNull();
         accession.PatientId.Should().BeNull();
+    }
+
+    [Test]
+    public void can_not_update_patient_when_processing_accession()
+    {
+        // Arrange
+        var patient = FakePatient.Generate(Mock.Of<IDateTimeProvider>());
+        var org = FakeHealthcareOrganization.Generate();
+        var accession = FakeAccessionBuilder
+            .Initialize()
+            .WithMockTestRepository()
+            .WithPatient(patient)
+            .WithHealthcareOrganization(org)
+            .Build();
+        var container = FakeContainer.Generate();
+        var sample = FakeSample.Generate(container);
+        accession.TestOrders.FirstOrDefault().SetSample(sample);
+        accession.DomainEvents.Clear();
+        accession.SetStatusToReadyForTesting(Mock.Of<IDateTimeProvider>());
+
+        var anotherPatient = FakePatient.Generate(Mock.Of<IDateTimeProvider>());
+        
+        // Act
+        var actAdd = () => accession.SetPatient(anotherPatient);
+        var actRemove = () => accession.RemovePatient();
+
+        // Assert
+        actAdd.Should().Throw<SharedKernel.Exceptions.ValidationException>()
+            .WithMessage($"This accession is processing. The patient can not be modified.");
+        actRemove.Should().Throw<SharedKernel.Exceptions.ValidationException>()
+            .WithMessage($"This accession is processing. The patient can not be modified.");
     }
 }
